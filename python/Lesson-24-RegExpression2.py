edca7206bd30bc51b758dc77d63b68d946b83244
tudorticau <tudorticau@mail.ru>
2022-12-07 13:54:57 +0200

    Lesson 23 - Regex - added examples to find emails

--- python/Lesson-24-RegExpression2.py
+++ python/Lesson-24-RegExpression2.py
@@ -1,24 +1,20 @@
 # import bibliography for regex
 import re
-# ---- Text where python will looking for ----------
-mytext = " Quon Phillips, tempor.diam.dictum@icloud.edu Hope Carey, facilisis@yahoo.net Norman Griffin,vulputate.lacus@outlook.org," \
-         " Germane Vaughan,sagittis@hotmail.org Yael Fleming,magna.tellus.faucibus@google.org" \
-         " Quon Phillips,(723) 464-1191,Jan 1, 2022 Hope Carey,1-312-473-9945,Nov 18, 2022" \
-         " Norman Griffin,1-728-660-1642, Aug 13, 2023 Germane Vaughan,(686) 883-9870,Jan 29, 2022" \
-         " Yael Fleming,1-281-625-7142,Dec 21, 2022"
-"""
-\d = Any Digit 0-9
-\D = Any non DIGIT
-\w = Any Alphabet symbol [A-Z a-z]
-\W = Any non Alphabet symbol
-\. = .
-\s = breakspace
-\S = non breakspace
-"""
+# ---- File where python will looking for ----------
+input_filename = "random_data.txt"
+result_filename = "results_data.txt"
+inputfile = open(input_filename, mode='r', encoding='Latin-1')
+resultfile = open(result_filename, mode='w', encoding='Latin-1')
+mytext = inputfile.read()
 
 # ---- program to find all emails ----
-textlookfor = r"\w+@\w+\.\w+"
-allresults = re.findall(textlookfor, mytext)
+lookfor = r"[\w.-]+@[\w.-]+"
 
-print(allresults)
+results = re.findall(lookfor, mytext)
+
+for item in results:
+    print(item)
+
+
+
 print("-------------------------------------------")
