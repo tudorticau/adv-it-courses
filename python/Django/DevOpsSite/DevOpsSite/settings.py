b4b472945c3d4cc8a78881395fe0d0c7d699dec3
tudorticau <tudorticau@mail.ru>
2022-12-08 13:47:19 +0200

    Lesson 35 -  configurate admin and default page

--- python/Django/DevOpsSite/DevOpsSite/settings.py
+++ python/Django/DevOpsSite/DevOpsSite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # My Web Pages will be listed here
+    "Ansible",
 ]
 
 MIDDLEWARE = [
